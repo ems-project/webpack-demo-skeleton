cf172954c59b2bbb93d8525fb0060083517c7175
theus deka <me@theus.be>
2019-08-19 23:25:16 +0200

    Add fortawesome

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@
 import './js/app';
 import './css/app.scss';
 
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+
 console.log('Hello world');
