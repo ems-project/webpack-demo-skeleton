31b52ed0e7d1697450dcb204ef5c208d6a0a9721
theus deka <me@theus.be>
2019-08-19 23:26:22 +0200

    CKEditor config

--- src/js/app.js
+++ src/js/app.js
@@ -4,11 +4,15 @@ window.$ = $;
 window.jQuery = $;
 
 require('bootstrap');
+
 import ace from 'ace-builds/src-noconflict/ace';
 import hashcash from '@elasticms/hashcash';
 
 const assetPath = document.querySelector("BODY").getAttribute('data-asset-path') ;
 ace.config.set('basePath', assetPath + 'js/ace' );
+window.CKEDITOR_BASEPATH = assetPath + 'js/ckeditor/';
+
+require('ckeditor');
 
 $( document ).ready(function() {
     const codeEditors = document.getElementsByClassName('ems-code-editor');
@@ -26,6 +30,98 @@ $( document ).ready(function() {
             theme: theme
         });
     }
+
+    $('.ckeditor_default').each(function(){
+        CKEDITOR.replace( $( this ).attr('id'), {
+            "plugins": "a11yhelp,basicstyles,bidi,blockquote,clipboard,colorbutton,colordialog,contextmenu,dialogadvtab,div,elementspath,enterkey,entities,filebrowser,find,floatingspace,format,horizontalrule,htmlwriter,image2,indentlist,indentblock,justify,language,list,liststyle,magicline,maximize,newpage,pastefromword,pastetext,preview,removeformat,resize,save,scayt,selectall,showborders,sourcearea,specialchar,stylescombo,tab,table,tabletools,templates,toolbar,undo,wsc,wysiwygarea",
+            "pasteFromWordRemoveFontStyles": true,
+            "pasteFromWordRemoveStyles": true,
+            "contentsCss": assetPath + 'css/index.css',
+            "height": 400,
+            "language": "en",
+            "toolbarGroups": [{
+                "name": "clipboard",
+                "groups": ["clipboard", "undo"]
+            }, {
+                "name": "clipboard",
+                "groups": ["clipboard", "undo"]
+            }, {
+                "name": "editing",
+                "groups": ["spellchecker"]
+            }, {
+                "name": "links"
+            }, {
+                "name": "insert"
+            }, {
+                "name": "tools",
+                "groups": ["mode"]
+            }, "/", {
+                "name": "basicstyles",
+                "groups": ["basicstyles", "cleanup"]
+            }, {
+                "name": "paragraph",
+                "groups": ["list", "indent", "blocks", "align"]
+            }, {
+                "name": "styles"
+            }],
+            "stylesSet": [{
+                "name": "Shell code",
+                "element": "pre",
+                "attributes" : {
+                    "class": "ems-code-editor",
+                    "data-language": "ace/mode/sh",
+                    "data-theme": "ace/theme/monokai",
+                    "data-max-lines": "400"
+                }
+            },{
+                "name": "YAML code",
+                "element": "pre",
+                "attributes" : {
+                    "class": "ems-code-editor",
+                    "data-language": "ace/mode/yaml",
+                    "data-theme": "ace/theme/monokai",
+                    "data-max-lines": "400"
+                }
+            },{
+                "name": "Apache Conf code",
+                "element": "pre",
+                "attributes" : {
+                    "class": "ems-code-editor",
+                    "data-language": "ace/mode/apache_conf",
+                    "data-theme": "ace/theme/monokai",
+                    "data-max-lines": "400"
+                }
+            },{
+                "name": "Twig code",
+                "element": "pre",
+                "attributes" : {
+                    "class": "ems-code-editor",
+                    "data-language": "ace/mode/twig",
+                    "data-theme": "ace/theme/monokai",
+                    "data-max-lines": "400"
+                }
+            },{
+                "name": "JSON code",
+                "element": "pre",
+                "attributes" : {
+                    "class": "ems-code-editor",
+                    "data-language": "ace/mode/json",
+                    "data-theme": "ace/theme/monokai",
+                    "data-max-lines": "400"
+                }
+            },{
+                "name": "Javascript code",
+                "element": "pre",
+                "attributes" : {
+                    "class": "ems-code-editor",
+                    "data-language": "ace/mode/javascript",
+                    "data-theme": "ace/theme/monokai",
+                    "data-max-lines": "400"
+                }
+            }]
+        });
+    });
+
 });
 
 
